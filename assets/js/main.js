81173357d899fd5f47bbb0dbbf9c047bd1d12b07
Benjamin Vuagnoux <vuagnoux.benjamin1@gmail.com>
2021-03-02 10:49:34 +0100

    ajout des fonctions de l'exercice 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,47 +15,47 @@ const exercice4return = document.getElementById("exercice4return");
 // DECLARATION DES FONCTIONS -------------------------------------------------------------------------
 // -- Exercice 1 - Calcul du nombre de jeunes, de moyens et de vieux.
 const jeunesMoyenVieux = () => {
-    let ages = new Array();
-    let input;
+    let ageArray = new Array();
+    let getAge;
     do {
-        input = window.prompt("Entrez un age à ajouter au compte, le comptage est arrété à la saisie d'un centenaire.", "");
-        // Est-ce que l'input est un nombre valid; 
-        while (isNaN(input)) {
-            input = window.prompt("Erreur : Entrez un age (nombre) à ajouter au compte, le comptage est arrété à la saisie d'un centenaire.", "");
+        getAge = window.prompt("Entrez un age à ajouter au compte, le comptage est arrété à la saisie d'un centenaire.", "");
+        // Est-ce que le getAge est un nombre valid; 
+        while (isNaN(getAge)) {
+            getAge = window.prompt("Erreur : Entrez un age (nombre) à ajouter au compte, le comptage est arrété à la saisie d'un centenaire.", "");
         };
         // break si l'utilisateur annule. 
-        if (ages.length == 0 && input === null) {
+        if (ageArray.length == 0 && getAge === null) {
             return false;
-        } else if (input === null || input === "") {
+        } else if (getAge === null || getAge === "") {
             break;
         };
         // Si tout est ok, on ajoute dans le tableau. 
-        ages.push(input);
-    } while (input < 100);
+        ageArray.push(getAge);
+    } while (getAge < 100);
     // comptage des jeunes, moyens, vieux.  
-    const jeunes = ages.filter(age => age < 20).length;
-    const moyens = ages.filter(age => age >= 20 && age < 40).length;
-    const vieux = ages.filter(age => age >= 40).length;
+    const jeunes = ageArray.filter(age => age < 20).length;
+    const moyens = ageArray.filter(age => age >= 20 && age < 40).length;
+    const vieux = ageArray.filter(age => age >= 40).length;
     // détails - au singulier ou au pluriel... 
-    let j = jeunes > 1 ? "jeunes" : "jeune";
-    let m = moyens > 1 ? "moyens" : "moyen";
+    let oneOrMoreJeunes = jeunes > 1 ? "jeunes" : "jeune";
+    let oneOrMoreMoyens = moyens > 1 ? "moyens" : "moyen";
     exercice1Btn.innerHTML = "Relancer la fonction";
     //valeur de retour
-    return exercice1return.innerHTML = `Parmi les ages que vous avez saisi vous avez : <br> ${jeunes} ${j} <br> ${moyens} ${m} <br> ${vieux} vieux.`;
+    return exercice1return.innerHTML = `Parmi les ages que vous avez saisi vous avez : <br> ${jeunes} ${oneOrMoreJeunes} <br> ${moyens} ${oneOrMoreMoyens} <br> ${vieux} vieux.`;
 };
 
 // Exercice 2 - Table de multiplication 
 const tableDeMultiplication = () => {
-    let multiple = parseInt(window.prompt("Entrez le multiple de la table à afficher", ""));
+    let getMultiple = parseInt(window.prompt("Entrez le multiple de la table à afficher", ""));
     //check si les valeurs sont valides
-    while (isNaN(multiple)) {
-        multiple = parseInt(window.prompt("Erreur : Entrez le multiple (nombre) de la table à afficher", ""));
+    while (isNaN(getMultiple)) {
+        getMultiple = parseInt(window.prompt("Erreur : Entrez le multiple (nombre) de la table à afficher", ""));
     };
     //reset le html du block de resultat
     exercice2return.innerHTML = "Votre table de multiplication : <br>";
     //boucle calcul de la table de multiplication 
     for (let i = 1; i <= 10; i++) {
-        exercice2return.innerHTML += (`${i} x ${multiple} = ${i * multiple} <br>`);
+        exercice2return.innerHTML += (`${i} x ${getMultiple} = ${i * getMultiple} <br>`);
     };
     //détails
     exercice2Btn.innerHTML = "Relancer la fonction";
@@ -64,19 +64,20 @@ const tableDeMultiplication = () => {
 
 // Ecercice 3 - recherche d'un prénom 
 const rechercheUnPrenom = () => {
-    let tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
-    console.log("🚀 ~ Tableau de départ", tab)
+    let baseTab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+    let baseTabCopy = [...baseTab];
+    console.log("🚀 ~ Tableau de départ", baseTabCopy)
     let input = window.prompt("Entrez un prénom", "");
-    let index = tab.indexOf(input);
-    // si prénom trouvé, on le suppr + case blanche a la fin de tab.
+    let index = baseTab.indexOf(input);
+    // si prénom trouvé, on le suppr + case blanche a la fin de baseTab.
     if (index >= 0) {
-        tab.splice(index, 1);
-        tab.push(' ');
+        baseTab.splice(index, 1);
+        baseTab.push(' ');
         exercice3return.innerHTML = "Appuyez sur F12 pour voir le resultat dans la console";
-        console.log(`Le prénom ${input} à bien été retiré de l'array de départ. Votre nouvel array :`, tab)
+        console.log(`Le prénom ${input} à bien été retiré de l'array de départ. Votre nouvel array :`, baseTab)
     } else {
-        exercice3return.innerHTML = "Ce prénom n'existe pas dans la liste";
-    }
+        exercice3return.innerHTML = `Le prénom ${input} n'existe pas dans la liste, attention aux majuscules et aux accents.`;
+    };
     //détails
     exercice3Btn.innerHTML = "Relancer la fonction";
 };
@@ -86,17 +87,65 @@ const rechercheUnPrenom = () => {
 const totalDUneCommande = () => {
     let PU = parseInt(window.prompt('Entrez le prix unitaire (PU)'));
     let QTECOM = parseInt(window.prompt('Entrez la quantité commandée (QTECOM)'));
+    //check si les valeurs sont correctes. 
+    while (isNaN(PU)) {
+        PU = parseInt(window.prompt('Erreur : Entrez le prix unitaire (PU)'));
+    };
+    while (isNaN(QTECOM)) {
+        QTECOM = parseInt(window.prompt('Erreur : Entrez la quantité commandée (QTECOM)'));
+    };
     let TOT = PU * QTECOM;
-    let remise;
-    let remisePourcent;
-    if (TOT >= 100 && TOT < 200) {
-        remise = 5;
-    } else if (TOT >= 100) {
-        remise = 10;
-    } else remise = 0;
-    // a terminer ... 
+    //préparation des variables pour le resultat. 
+    let remisePourcent = getRemise(TOT).pourcentage;
+    let totalRemise = TOT * getRemise(TOT).multiple;
+    let totalRemiseEur = TOT - totalRemise;
+    let portPourcent = getPort(totalRemise).pourcentage;
+    let totalPort = TOT * getPort(totalRemise).multiple;
+    //On applique un FDP mini de 6€
+    if (totalRemise < 500 && totalPort < 6) {
+        totalPort = 6;
+        portPourcent = "6€ (frais minimum à payer)"
+    };
+    let totalAPayer = totalRemise + totalPort;
+    //Résultat. 
+    exercice4return.innerHTML = `PU = ${PU} <br> Quantité = ${QTECOM} <hr> remise = ${remisePourcent} (-${totalRemiseEur.toFixed(1)}€) soit ${totalRemise.toFixed(2)}€ <br> frais de port = ${portPourcent} <hr> À payer ${totalAPayer}€.`
+};
 
-}
+const getRemise = (totalHorsRemise) => {
+    let remise;
+    if (totalHorsRemise >= 100 && totalHorsRemise <= 200) {
+        remise = {
+            pourcentage: "5%",
+            multiple: 0.95
+        };
+    } else if (totalHorsRemise >= 200) {
+        remise = {
+            pourcentage: "10%",
+            multiple: 0.90
+        };
+    } else remise = {
+        pourcentage: "0%",
+        multiple: 1
+    };
+    return remise;
+};
+
+const getPort = (totalRemise) => {
+    let port;
+    if (totalRemise >= 500) {
+        port = {
+            pourcentage: "0%",
+            multiple: 0
+        }
+    } else {
+        port = {
+            pourcentage: "2%",
+            multiple: 0.02
+        }
+    };
+    return port;
+};
+
 
 // GESTION DES EVENEMENTS ------------------------------------------------------------------------- 
 // -- Exercice 1
